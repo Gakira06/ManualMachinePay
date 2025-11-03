0d544a998936627e30bf2b44f31f917d06189b4e
Gabriel <gakirakiyohara@gmail.com>
2025-11-03 11:14:16 -0300

    fix: Arrumando config do vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/MachinePay.github.io/', // 👈 coloque exatamente o nome do seu repositório aqui
+  plugins: [react(), tailwindcss()],
 })
